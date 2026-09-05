5bd92157f0ed890595cedb119e2ffe7f5cd19632
Vladimir Ivanov <elitic.pantheism@gmail.com>
2026-09-05 21:52:27 +0300

    Use Vercel native Next.js output

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  ...(process.env.VERCEL ? {} : { output: 'standalone' }),
   experimental: {
     useTypeScriptCli: false
   }
